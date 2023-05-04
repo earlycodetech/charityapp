c32fec851a59aaa6f8ef8f9bf003095fb59a3626
Joseph Ogbu <fallyfox@gmail.com>
2023-05-04 17:57:54 +0100

    initial home screen build

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+View,
+Text,
+StyleSheet,
+TouchableOpacity,
+FlatList,
+Image,
+SafeAreaView,
+Platform,
+StatusBar } from "react-native";
 
-export default function App() {
+export default function App () {
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <View style={styles.leftHeader}>
+          <Image 
+          source={require('./assets/charityapp.png')}
+          alt='app logo'
+          style={styles.logo}/>
+          <Text style={styles.brandName}>CharityApp</Text>
+        </View>
+
+        <Image 
+        source={require('./assets/user.png')}
+        alt='icon'
+        style={styles.headerIcon}/>
+      </View>
+
+      <View style={styles.middleContainer}>
+        <View style={styles.actionBox}>
+          
+        </View>
+      </View>
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    marginTop:Platform.OS == 'android' ? StatusBar.currentHeight : 0,
+    paddingHorizontal:8
   },
-});
+  header:{
+    flexDirection:'row',
+    justifyContent:'space-between',
+  },
+  leftHeader:{
+    flexDirection:'row',
+    alignItems:'center'
+  },
+  logo:{
+    width:52,
+    height:52,
+    marginRight:4
+  },
+  brandName:{
+    fontSize:28,
+    fontWeight:'bold',
+    color:'red'
+  },
+  headerIcon:{
+    width:48,
+    height:48
+  }
+})
